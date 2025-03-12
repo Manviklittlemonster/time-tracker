6876058be780a054f1bfb7bd6dfe4f7342ea1f71
Manviklittlemonster <Maneesh4_u@yahoo.co.in>
2025-03-12 10:24:06 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -2,6 +2,7 @@ package training.taylor.timetracker.core;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
-/**
- * Created by Jason on 6/19/2015.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TrackerCoreConfig.class)
 public class TrackerTest {
     @Autowired
     private Tracker tracker;
+
+    @Before
+    public void setUp() {
+        // Clear the tracker before each test
+        while (tracker.size() > 0) {
+            tracker.remove(tracker.get(0));
+        }
+    }
 
     @Test
     public void testMe() {
@@ -32,4 +38,27 @@ public class TrackerTest {
         tracker.add(entry);
         assertTrue(tracker.size() > 0);
     }
+
+    @Test
+    public void testRemove() {
+        TimeEntry entry = new TimeEntry();
+        entry.setDescription("Entry Test");
+        tracker.add(entry);
+        tracker.remove(entry);
+        assertEquals(0, tracker.size());
+    }
+
+    @Test
+    public void testGet() {
+        TimeEntry entry = new TimeEntry();
+        entry.setDescription("Entry Test");
+        tracker.add(entry);
+        assertEquals(entry, tracker.get(0));
+    }
+
+    @Test
+    public void testEmptyTracker() {
+        assertEquals(0, tracker.size());
+    }
 }
+
